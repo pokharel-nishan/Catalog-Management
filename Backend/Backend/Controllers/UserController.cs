73586441d33f4a0027534b3fe556e5c05135b525
Nishan Pokharel <itsmenishan.p@gmail.com>
2025-04-17 08:08:06 +0545

    [setup] Reconfigured the backend setup.

--- Backend/Backend/Controllers/UserController.cs
+++ Backend/Backend/Controllers/UserController.cs
@@ -4,21 +4,14 @@ namespace Backend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class UserController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
+    
+    [HttpGet(Name = "GetUser")]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
