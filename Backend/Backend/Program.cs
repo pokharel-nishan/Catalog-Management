7fc14c8564be7c73dabf368bf991f27a1f7f369e
Nishan Pokharel <itsmenishan.p@gmail.com>
2025-04-23 15:42:08 +0545

    [add] added relevant config for creating Identity tables in the database.

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,3 +1,7 @@
+using Backend;
+using Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<User, Role>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
