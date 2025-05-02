ec35ecdb11d3895ea7307746e0a90c723bece5bb
Susan Pokharel <pokharelj120@gmail.com>
2025-05-02 21:57:47 +0545

    generated migration for bookmark-book table.

---
+++ Backend/Backend/Migrations/20250502161152_BookmarkBookTableCreation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookmarkBookTableCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BookmarkBooks",
+                columns: table => new
+                {
+                    BookmarkId = table.Column<Guid>(type: "uuid", nullable: false),
+                    BookId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookmarkBooks", x => new { x.BookmarkId, x.BookId });
+                    table.ForeignKey(
+                        name: "FK_BookmarkBooks_Bookmarks_BookmarkId",
+                        column: x => x.BookmarkId,
+                        principalTable: "Bookmarks",
+                        principalColumn: "BookmarkId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookmarkBooks_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookmarkBooks_BookId",
+                table: "BookmarkBooks",
+                column: "BookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookmarkBooks");
+        }
+    }
+}
