cbc33e90aeb25e81c63ecf21d4cc4b9785a334c5
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-05-03 23:32:02 +0545

    added AddBookDTO, UpdateBookDTO, and BookRepo, IBookRepo in Backend/

---
+++ Backend/Backend/DTOs/Admin/Book/AddBookDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.DTOs.Admin.Book
+{
+    public class AddBookDTO
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+        public string Format { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
+        public float Discount { get; set; }
+
+    }
+}
---
+++ Backend/Backend/Repositories/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿//using Backend.DTOs.Admin.Book;
+
+//namespace Backend.Repositories
+//{
+//    public class BookRepository: IBookRepository
+//    {
+//        public string AddBook(AddBookDTO addBookDTO)
+//        {
+//            try
+//            {
+
+//            }
+//            catch (Exception ex)
+//            {
+//                return (false, null)
+//            }
+//    }
+//}
