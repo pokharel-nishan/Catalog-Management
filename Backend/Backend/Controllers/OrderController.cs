e41b903fb6ad3a7d8311f4ed6a022ce3f59224ac
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-05-05 16:29:45 +0545

    [add] added functionality to process order claim codes

---
+++ Backend/Backend/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Backend.DTOs.Admin.Book;
+using Backend.Entities;
+using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    [HttpPost("processClaimCode/{orderId}")]
+    //[Authorize(Roles = "Staff")]
+    public async Task<IActionResult> ProcessClaimCodeAsync(Guid orderId, string claimCode)
+    {
+        var isClaimCodeValid = await _orderService.ProcessClaimCodeAsync(orderId, claimCode);
+
+        if (!isClaimCodeValid)
+        {
+            return Ok($"Success: Order {orderId} processed successfully!");
+        }
+        return BadRequest($"Error: Invalid claim code for order {orderId}!");
+    }
+}
---
+++ Backend/Backend/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Repositories;
+
+namespace Backend.Services
+{
+    public class OrderService : IOrderService
+    {
+        private IOrderRepository _orderRepository;
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<bool> ProcessClaimCodeAsync(Guid orderId, string claimCode)
+        {
+            var order = await _orderRepository.GetOrderByIdAsync(orderId);
+
+            if (order == null)
+            {
+                var orderClaimCode = order.ClaimCode;
+                if (orderClaimCode != null)
+                {
+                    return orderClaimCode == claimCode;
+                }
+                Console.WriteLine("No claim code found!");
+                return false;
+            }
+            Console.WriteLine("Order does not exist!");
+            return false;
+        }
+    }
+}
