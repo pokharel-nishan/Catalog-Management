1f2c3c770b91f1a8c92b48870d25fc29e6b5c9d1
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-05-08 17:14:15 +0545

    [add] added functions to send emails to user and staff users after order cancellation

--- Backend/Backend/Repositories/IUserRepository.cs
+++ Backend/Backend/Repositories/IUserRepository.cs
@@ -15,5 +15,7 @@ public interface IUserRepository
 
     Task<User> CreateStaffUserAsync(User user, string password);
 
+    Task<List<User>> GetAllStaffUsersAsync();
+
 
 }
--- Backend/Backend/Services/OrderService.cs
+++ Backend/Backend/Services/OrderService.cs
@@ -10,11 +10,13 @@ namespace Backend.Services
     {
         private IOrderRepository _orderRepository;
         private IOrderBookRepository _orderBookRepository;
+        private IUserRepository _userRepository;
         private EmailService _emailService;
-        public OrderService(IOrderRepository orderRepository, IOrderBookRepository orderBookRepository, EmailService emailService)
+        public OrderService(IOrderRepository orderRepository, IOrderBookRepository orderBookRepository, IUserRepository userRepository, EmailService emailService)
         {
             _orderRepository = orderRepository;
             _emailService = emailService;
+            _userRepository = userRepository;
             _orderBookRepository = orderBookRepository;
         }
 
@@ -75,7 +77,7 @@ namespace Backend.Services
                 emailBody.AppendLine($"<p><strong>Total Discount:</strong> {order.Discount * 100}</p>");
                 emailBody.AppendLine($"<h3>Grand Total: {order.TotalPrice:C}</h3>");
 
-                string emailSubject = $"Invice for Order {order.OrderId}";
+                string emailSubject = $"Invoice for Order {order.OrderId}";
                 string recipientEmail = order.User.Email;
 
                 await _emailService.SendEmailAsync(recipientEmail, emailSubject, emailBody.ToString());
@@ -83,6 +85,64 @@ namespace Backend.Services
             catch (Exception ex)
             {
                 Console.WriteLine($"Error sending order confirmation email: {ex.Message}");
+            }
+        }
+
+
+        private async Task SendOrderCancellationEmail(Order order)
+        {
+            try
+            {
+                // Get Order Books
+                var orderBooks = await _orderBookRepository.GetOrderBooksAsync(order.OrderId);
+
+                // Build the invoice
+                // Order Details
+                StringBuilder emailBody = new StringBuilder();
+                emailBody.AppendLine($"<h2>Invoice for Order: {order.OrderId}</h2>");  // Header
+                emailBody.AppendLine($"<p><strong>Claim Code:</strong> {order.ClaimCode}</p>");  // Claim Code
+                emailBody.AppendLine($"<p><strong>Order Date:</strong> {order.OrderDate}</p>");  // Order Date
+                emailBody.AppendLine($"<p><strong>Status:</strong> {order.Status}</p>");  // Order Status
+
+                emailBody.AppendLine($"<h3>Order Details:</h3>");
+
+                // Book Details
+                foreach (var orderBook in orderBooks)
+                {
+                    var title = orderBook.Book.Title;
+                    int quantity = orderBook.BookQuantity;
+                    var unitPrice = orderBook.Book.Price;
+                    var bookDiscount = orderBook.Book.Discount;
+                    var discountedBookPrice = unitPrice * (1 - bookDiscount);
+                    var subtotal = discountedBookPrice * quantity;
+
+                    emailBody.AppendLine($"<p><strong>Book Title:</strong> {title}</p>");  // Book Title
+                    emailBody.AppendLine($"<p>Quantity: {quantity}</p>");  // Quantity
+                    emailBody.AppendLine($"<p>Unit Price: {unitPrice:C}</p>");  // Unit Price
+                    emailBody.AppendLine($"<p>Book Discount: {bookDiscount * 100}%</p>");  // Book Discount (Individual)
+                    emailBody.AppendLine($"<p><strong>Subtotal:</strong> {subtotal:C}</p><hr>");  // Book Subtotal
+                }
+
+                // Total Quantity, Total Discount, Total Price
+                emailBody.AppendLine($"<p><strong>Total Quantity:</strong> {order.TotalQuantity}</p>");
+                emailBody.AppendLine($"<p><strong>Total Discount:</strong> {order.Discount * 100}</p>");
+                emailBody.AppendLine($"<h3>Grand Total: {order.TotalPrice:C}</h3>");
+
+                string emailSubjectForUser = $"Your Order {order.OrderId} has been cancelled.";
+                string emailSubjectForStaff = $"Order {order.OrderId} has been cancelled.";
+
+                var recipientUserEmail = order.User.Email;
+                await _emailService.SendEmailAsync(recipientUserEmail, emailSubjectForUser, emailBody.ToString());
+
+                var staffUsers = await _userRepository.GetAllStaffUsersAsync();
+                foreach (var staffUser in staffUsers)
+                {
+                    await _emailService.SendEmailAsync(staffUser.Email, emailSubjectForStaff, emailBody.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending order cancellation email: {ex.Message}");
             }
         }
     }
