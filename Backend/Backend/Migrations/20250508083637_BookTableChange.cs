e1c8f7f7659b867ebf44af718c6c787fc1cd0d61
Shirshak52 <np01cp4s230122@islingtoncollege.edu.np>
2025-05-08 14:36:11 +0545

    [add] added discount start and end dates and arrival date in book entity

---
+++ Backend/Backend/Migrations/20250508083637_BookTableChange.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookTableChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArrivalDate",
+                table: "Books",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DiscountEndDate",
+                table: "Books",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DiscountStartDate",
+                table: "Books",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArrivalDate",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountEndDate",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountStartDate",
+                table: "Books");
+        }
+    }
+}
